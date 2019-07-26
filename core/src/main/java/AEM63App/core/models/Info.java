cef6304dd064a6f694466608b1388a650c230c08
Ramiro Matteoda <ramiro.matteoda@3sharecorp.com>
2019-07-26 16:31:27 -0300

    HTL and dialog training session

---
+++ core/src/main/java/AEM63App/core/models/Info.java
@@ -0,0 +1,22 @@
+package AEM63App.core.models;
+
+import com.adobe.cq.sightly.WCMUsePojo;
+ 
+public class Info extends WCMUsePojo {
+    private String lowerCaseTitle;
+    private String lowerCaseDescription;
+ 
+    @Override
+    public void activate() throws Exception {
+        lowerCaseTitle = getProperties().get("title", "").toLowerCase();
+        lowerCaseDescription = getProperties().get("description", "").toLowerCase();
+    }
+ 
+    public String getLowerCaseTitle() {
+        return lowerCaseTitle;
+    }
+ 
+    public String getLowerCaseDescription() {
+        return lowerCaseDescription;
+    }
+}
--- ui.apps/src/main/content/jcr_root/apps/sling/servlet/errorhandler/ResponseStatus.java
+++ ui.apps/src/main/content/jcr_root/apps/sling/servlet/errorhandler/ResponseStatus.java
@@ -15,13 +15,14 @@
  */
 package apps.sling.servlet.errorhandler;
 
-import com.adobe.cq.sightly.WCMUse;
+//import com.adobe.cq.sightly.WCMUse;
 
-public class ResponseStatus extends WCMUse {
+public class ResponseStatus {
+//    public class ResponseStatus extends WCMUse {
     
-    @Override
-    public void activate() throws Exception {
-        getResponse().setStatus(404);
-        getResponse().setContentType("text/html");
-    }
+   //@Override
+   // public void activate() throws Exception {
+   //     getResponse().setStatus(404);
+   //     getResponse().setContentType("text/html");
+   // }
 }
